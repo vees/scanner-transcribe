8653172ccb6edb9783d24a66d582b57f4e614e25
Rob Carlson <rob@vees.net>
2018-06-17 22:01:22 -0400

    Convert RecognizeResponse to JSON for transmit to web site

---
+++ recognize_to_json.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 17 21:19:40 2018
+
+@author: rob
+"""
+
+"""
+type(f)
+Out[38]: google.cloud.speech_v1.types.RecognizeResponse
+
+type(f.results)
+Out[44]: google.protobuf.pyext._message.RepeatedCompositeContainer
+
+type(f.results[0])
+Out[45]: google.cloud.speech_v1.types.SpeechRecognitionResult
+
+type(f.results[0].alternatives)
+Out[46]: google.protobuf.pyext._message.RepeatedCompositeContainer
+
+type(f.results[0].alternatives[0])
+Out[47]: google.cloud.speech_v1.types.SpeechRecognitionAlternative
+
+type(f.results[0].alternatives[0].transcript)
+Out[48]: str
+
+"""
+
+def to_json(g):
+    from google.cloud.speech_v1 import types
+    import json
+    import os
+    from datetime import datetime
+    if not isinstance(g,types.RecognizeResponse):
+        return {}
+    file='/home/rob/scanner/scanner-2018-06-17-20-51-06.wav'
+    base=os.path.basename(file)
+    begintime=datetime.strptime(base,"scanner-%Y-%m-%d-%H-%M-%S.wav").isoformat()
+    a=[]
+    i=0
+    for result in g.results:
+        i+=1
+        if len(result.alternatives)>0:
+            alt=result.alternatives[0]
+           
+            a.append({'recordtime': begintime,
+                      'sequence': i,
+                      'transcript': alt.transcript, 
+                      'confidence': alt.confidence})
+    
+    return json.dumps(a)
